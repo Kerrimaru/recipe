31e3bdee0efd5ffd9ec2a885ce80e2ab434be6f9
Kerri Marusiak <kerri.maru@gmail.com>
2021-11-24 18:09:57 +0000

    first checkobox complete v11

--- src/app/recipes/nutrition-table/nutrition-table.component.spec.ts
+++ src/app/recipes/nutrition-table/nutrition-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NutritionTableComponent } from './nutrition-table.component';
 
@@ -6,7 +6,7 @@ describe('NutritionTableComponent', () => {
   let component: NutritionTableComponent;
   let fixture: ComponentFixture<NutritionTableComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NutritionTableComponent ]
     })
