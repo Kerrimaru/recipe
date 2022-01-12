f0bd4d5802f3ce2922d529cc9d0d60565469b03f
Kerri Marusiak <kerri.maru@gmail.com>
2022-01-12 18:18:14 +0000

    create datelist comp, month and reverse pipe

---
+++ src/app/shared/pipes/month.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "month",
+})
+export class MonthPipe implements PipeTransform {
+  transform(value: number): string {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return months[value];
+  }
+}
